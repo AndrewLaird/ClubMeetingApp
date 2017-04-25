865f6401712f0a3fb431dc7c299dcf82bfc9c1c8
thantos138 <bssoohoo@uci.edu>
2017-04-24 20:02:02 -0700

    Not sure what happened......

--- LandingPage.php
+++ LandingPage.php
@@ -1,8 +1,14 @@
+<html>
 <head>
 	<link rel = 'stylesheet' href='LandingPage.css'>
 </head>
-<div id = "title">This is just a start</div>
+<div id = "title">This is just a start
 
 <?php
-	echo("Hello World");
+echo "Hello World"
 ?>
+
+</div>
+
+
+</html>
